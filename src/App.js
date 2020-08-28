35b65c393076c96408750d4f128526cd9dc5a96e
Irene <airine@Irenes-MacBook-Air.local>
2020-08-28 14:12:42 -0700

    [Components] Add blog,about,project component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,24 @@ import {
 } from 'react-router-dom';
 
 import Homepage from './Homepage'
+import About from './About'
+import Blog from './Blog'
+import Project from './Project'
 
 function App() {
   return (
     <Router>
     <div className="App">
       <nav>
-        <Link to="/">Homepage</Link> 
+        <Link to="/">Homepage</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/project">Project</Link>
       </nav>
       <Route exact path="/" component={Homepage} />
+      <Route path="/about" component={About} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/project" component={Project} />
     </div>
     </Router>
   );
