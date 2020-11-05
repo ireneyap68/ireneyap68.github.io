fcfcc5cfc856473efc205796672bdc3029292efb
Irene <airine@Irenes-MacBook-Air.local>
2020-11-04 22:19:59 -0800

    Updates

---
+++ precache-manifest.32a114195b1631d4ef2749850d70cd91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046f6b9b3c0571a99669896dd130e895",
+    "url": "/portfolio-react-router/index.html"
+  },
+  {
+    "revision": "d3cfb7e9d2e13ce7c19b",
+    "url": "/portfolio-react-router/static/css/main.09149c08.chunk.css"
+  },
+  {
+    "revision": "f3aeca442edf7414bf53",
+    "url": "/portfolio-react-router/static/js/2.6a8922bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react-router/static/js/2.6a8922bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cfb7e9d2e13ce7c19b",
+    "url": "/portfolio-react-router/static/js/main.36ded5b9.chunk.js"
+  },
+  {
+    "revision": "aa7a5f79d4715e7fc423",
+    "url": "/portfolio-react-router/static/js/runtime-main.a11495df.js"
+  },
+  {
+    "revision": "1cc4294ee4f88a0d2bdda42a0ca4dfcf",
+    "url": "/portfolio-react-router/static/media/aboutLogo.1cc4294e.JPG"
+  },
+  {
+    "revision": "e1db45f06bd8cd6cf30503b1eae4d069",
+    "url": "/portfolio-react-router/static/media/portfolio1.e1db45f0.jpg"
+  }
+]);
