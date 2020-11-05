4209f1908e2f5367010bce464d11b48623ab6423
Irene <airine@Irenes-MacBook-Air.local>
2020-11-04 22:50:17 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Welcome to my Portfolio
-
-![Home](./src/images/power4.jpg)
-
-## Please visit: https://ireneyap68.github.io/portfolio-react-router/
---
+++ precache-manifest.4a19c901d505e6cc39f9870146146d16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3666e0e549a12c0f4face30d2802329b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4b8354591f7a39eaed",
+    "url": "/static/css/main.09149c08.chunk.css"
+  },
+  {
+    "revision": "262a9ee094ba1ac87b3c",
+    "url": "/static/js/2.0b8a431e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0b8a431e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4b8354591f7a39eaed",
+    "url": "/static/js/main.dc0ea455.chunk.js"
+  },
+  {
+    "revision": "1f7cb09ef3eea260c7ef",
+    "url": "/static/js/runtime-main.e4113326.js"
+  },
+  {
+    "revision": "1cc4294ee4f88a0d2bdda42a0ca4dfcf",
+    "url": "/static/media/aboutLogo.1cc4294e.JPG"
+  },
+  {
+    "revision": "e1db45f06bd8cd6cf30503b1eae4d069",
+    "url": "/static/media/portfolio1.e1db45f0.jpg"
+  }
+]);
